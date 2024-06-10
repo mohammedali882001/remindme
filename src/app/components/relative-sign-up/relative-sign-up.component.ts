5aa390c349f33807dd3ece83e9f2464ab27a58f5
Madonna Adel <madonnadel0@gmail.com>
2024-06-11 01:03:49 +0300

    push the Register form

---
+++ src/app/components/relative-sign-up/relative-sign-up.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-relative-sign-up',
+  standalone: true,
+  imports: [],
+  templateUrl: './relative-sign-up.component.html',
+  styleUrl: './relative-sign-up.component.css'
+})
+export class RelativeSignUpComponent {
+
+}
