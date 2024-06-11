fddae6d10c749bba54988358f54606fa35c228b8
salmahammad436 <sayahammad@gmail.com>
2024-06-11 03:17:15 +0300

    salma updates

---
+++ src/app/components/NavBar/nav-bar/nav-bar.component.ts
@@ -0,0 +1,20 @@
+
+import { CommonModule } from '@angular/common';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+  @ViewChild('navbar') navbar!: ElementRef;
+  isNavbarCollapsed = true;
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  }
+
