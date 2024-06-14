2ae0940192c6eb74eda89db922e768859432d747
salmahammad436 <sayahammad@gmail.com>
2024-06-15 02:35:03 +0300

    docprofile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavBarComponent } from './components/NavBar/nav-bar/nav-bar.component';
 import { FooterComponent } from './components/Footer/footer/footer.component';
 import { CardComponent } from './components/Card/card/card.component';
 import { RouterModule } from '@angular/router';
+import { DoctorprofileComponent } from './pages/DoctorProfile/doctorprofile/doctorprofile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,NavBarComponent,FooterComponent ,CardComponent,RouterModule],
+  imports: [RouterOutlet,NavBarComponent,FooterComponent ,CardComponent,RouterModule,DoctorprofileComponent],
 
 templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/pages/DoctorProfile/doctorprofile/doctorprofile.component.ts
+++ src/app/pages/DoctorProfile/doctorprofile/doctorprofile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import {SidebarComponent} from '../../../components/drawer/sidebar/sidebar.component'
+import {StarRatingComponent} from '../../../components/Ranting/star-rating/star-rating.component'
 @Component({
   selector: 'app-doctorprofile',
   standalone: true,
-  imports: [],
+  imports: [SidebarComponent,StarRatingComponent],
   templateUrl: './doctorprofile.component.html',
   styleUrl: './doctorprofile.component.css'
 })
