645bd8de998b1b46b09b8e0fc9c61410c967f9f7
Madonna Adel <madonnadel0@gmail.com>
2024-06-15 11:35:00 +0300

    Rest-password created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { DoctorSignUpComponent } from './components/doctor-sign-up/doctor-sign-up.component';
+import { RestPasswordComponent } from './components/rest-password/rest-password.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, LandingComponent,
-    DoctorSignUpComponent,LoginComponent
+    DoctorSignUpComponent,LoginComponent,RestPasswordComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
