7f676aaa34bd9c10c4d49b678c24a4f51cd1128f
Habeba-Adnan <Habebaadnan2020@gmail.com>
2024-06-19 02:56:04 +0300

    end routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
+//import { RegisterComponent } from './register/register.component';
+import { DoctorprofileComponent } from './pages/DoctorProfile/doctorprofile/doctorprofile.component';
 
 export const routes: Routes = [
 
 { path: '', redirectTo: 'landing', pathMatch: 'full' },
 { path: 'landing', component: LandingComponent },
 {path:'login',component:LoginComponent},
+//{path:'register',component:RegisterComponent},
+{path:'doctor',component:DoctorprofileComponent},
+
+
 
 ];
