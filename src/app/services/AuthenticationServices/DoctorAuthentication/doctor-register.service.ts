b1201d4df681229751dee7eb088ead281b34b8ac
Habeba-Adnan <Habebaadnan2020@gmail.com>
2024-06-19 08:34:10 +0300

    fix

---
+++ src/app/services/AuthenticationServices/DoctorAuthentication/doctor-register.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+//import { environment } from '../../../../environments/environment.development';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DoctorRegisterService {
+ // private apiUrl = `${environment.baseUrl}/Account/Register/Doctor`;
+  private apiUrl = `http://localhost:2100/api/Account/Register/Doctor`;
+
+  constructor(private _httpClient:HttpClient){}
+  registerDoctor(registerDto: any): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this._httpClient.post(this.apiUrl, registerDto, { headers });
+  }
+}
