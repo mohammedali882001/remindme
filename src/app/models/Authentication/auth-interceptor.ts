cdc0dec99586b79c306a498b6d084817ebf9f729
Habeba-Adnan <Habebaadnan2020@gmail.com>
2024-06-21 14:00:44 +0300

    test

--- src/app/models/Authentication/auth-interceptor.ts
+++ src/app/models/Authentication/auth-interceptor.ts
@@ -7,7 +7,7 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('token');
     if (token) {
-      console.log('Interceptor triggered, token:', token); // Log for debugging
+      console.log('Interceptor triggered, token:', token); 
       const authReq = req.clone({
         setHeaders: {
           Authorization: `Bearer ${token}`
