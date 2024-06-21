7c8e3cf9f6332be4f2d736fc7f3e7a18ba641e0c
Mohammed Ali <93165125+mohammedali882001@users.noreply.github.com>
2024-06-20 17:16:23 -0800

    edit username in doctor module

--- src/app/services/AuthenticationServices/DoctorAuthentication/doctor-register.service.ts
+++ src/app/services/AuthenticationServices/DoctorAuthentication/doctor-register.service.ts
@@ -5,24 +5,22 @@ import { environment } from '../../../../environments/environment.development';
 import { Observable, map } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DoctorRegisterService {
   private apiUrl = `${environment.baseUrl}/Account/Register/Doctor`;
 
-
-  constructor(private _httpClient:HttpClient){}
+  constructor(private _httpClient: HttpClient) {}
   registerDoctor(registerDto: any): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
     return this._httpClient.post(this.apiUrl, registerDto, { headers });
   }
 
-  isUsernameTaken(userName: string): Observable<boolean> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    const checkUsernameDto = { userName };
-    return this._httpClient.post<any>(`${this.apiUrl}/Account/CheckUsername`, checkUsernameDto, { headers }).pipe(
-      map(response => !response.isSuccess) // `true` if taken, `false` if available
-    );
-  }
-
+  // isUsernameTaken(userName: string): Observable<boolean> {
+  //   const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+  //   const checkUsernameDto = { userName };
+  //   return this._httpClient.post<any>(`${this.apiUrl}/Account/CheckUsername`, checkUsernameDto, { headers }).pipe(
+  //     map(response => !response.isSuccess) // `true` if taken, `false` if available
+  //   );
+  // }
 }
--- src/app/validators/unique-username-validator.validator.ts
+++ src/app/validators/unique-username-validator.validator.ts
@@ -1,13 +1,19 @@
-import { AbstractControl, AsyncValidatorFn, ValidationErrors } from "@angular/forms";
-import { DoctorRegisterService } from "../services/AuthenticationServices/DoctorAuthentication/doctor-register.service";
-import { Observable, catchError, map, of, switchMap } from "rxjs";
+import {
+  AbstractControl,
+  AsyncValidatorFn,
+  ValidationErrors,
+} from '@angular/forms';
+import { DoctorRegisterService } from '../services/AuthenticationServices/DoctorAuthentication/doctor-register.service';
+import { Observable, catchError, map, of, switchMap } from 'rxjs';
 
-export function uniqueUsernameValidator(doctorRegisterService: DoctorRegisterService): AsyncValidatorFn {
-  return (control: AbstractControl): Observable<ValidationErrors | null> => {
-    return control.valueChanges.pipe(
-      switchMap(value => doctorRegisterService.isUsernameTaken(value)),
-      map(isTaken => (isTaken ? { notUnique: true } : null)),
-      catchError(() => of(null))
-    );
-  };
-}
+// export function uniqueUsernameValidator(
+//   doctorRegisterService: DoctorRegisterService
+// ): AsyncValidatorFn {
+//   // return (control: AbstractControl): Observable<ValidationErrors | null> => {
+//   //   return control.valueChanges.pipe(
+//   //     switchMap(value => doctorRegisterService.isUsernameTaken(value)),
+//   //     map(isTaken => (isTaken ? { notUnique: true } : null)),
+//   //     catchError(() => of(null))
+//   //   );
+//   // };
+// }
