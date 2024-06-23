df44219aa0537f59baafa5623efd25aa82bcf3e8
Habeba-Adnan <Habebaadnan2020@gmail.com>
2024-06-24 02:57:55 +0300

    edit appointments

--- src/app/pages/DoctorProfile/doctorprofile/doctorprofile.component.ts
+++ src/app/pages/DoctorProfile/doctorprofile/doctorprofile.component.ts
@@ -46,57 +46,18 @@ export class DoctorprofileComponent implements OnInit {
 
   fetchAvailableSlots(): void {
     this.doctorProfileService.getAvailableSlots().subscribe({
-      next: (slots: AvailableSlotsDTO[]) => {
-        console.log("Sloots",slots);
-
-        this.availableSlots = slots;
-        console.log("AvaSloots",this.availableSlots);
-
+      next: (response) => {
+        if (response.isSuccess) {
+          this.availableSlots = response.data;
+          console.log("Available Slots", this.availableSlots);
+        } else {
+          console.error('Error: Unsuccessful response', response);
+        }
       },
-      error: (error:any) => {
+      error: (error) => {
         console.error('Error fetching available slots:', error);
       }
     });
   }
 }
 
-// export class DoctorprofileComponent implements OnInit {
-//   profileData: any;
-//   availableSlots: AvailableSlotsDTO[] = [];
-//   errorMessage: string = '';
-
-//   constructor(private doctorProfileService: DoctorProfileService) {}
-
-//   ngOnInit(): void {
-//     this.doctorProfileService.getProfile().subscribe({
-//       next: (data) => {
-//         this.profileData = data.data;
-//         this.fetchAvailableSlots();
-//       },
-//       error: (error) => {
-//         console.error('Error fetching profile data:', error);
-//         if (error.status === 401) {
-//           this.errorMessage = 'Unauthorized access. Please login.';
-//         } else {
-//           this.errorMessage = 'Error fetching profile data. Please try again later.';
-//         }
-//       }
-//     });
-//   }
-
-//   fetchAvailableSlots(): void {
-//     this.doctorProfileService.getAvailableSlots().subscribe({
-//       next: (slots) => {
-//         this.availableSlots = slots;
-//       },
-//       error: (error) => {
-//         console.error('Error fetching available slots:', error);
-//         if (error.status === 403) {
-//           this.errorMessage = 'Forbidden access. You do not have permission to access this resource.';
-//         } else {
-//           this.errorMessage = 'Error fetching available slots. Please try again later.';
-//         }
-//       }
-//     });
-//   }
-// }
