3bcd8125c59fb399113ae67396e9424dffda1f36
Kerollos-Wessa <kerollos.wessa21@gmail.com>
2024-06-26 02:35:47 +0300

    donight fix

--- src/app/pages/DoctorDashboard/doctor-dashboard/doctor-dashboard.component.ts
+++ src/app/pages/DoctorDashboard/doctor-dashboard/doctor-dashboard.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 import {SidebarComponent} from '../../../components/drawer/sidebar/sidebar.component'
-import {DonightChartComponent} from '../../../components/DonughtChart/donight-chart/donight-chart.component'
+//import {DonightChartComponent} from '../../../components/DonughtChart/donight-chart/donight-chart.component'
 
 @Component({
   selector: 'app-doctor-dashboard',
   standalone: true,
-  imports: [SidebarComponent,DonightChartComponent],
+  imports: [SidebarComponent],
   templateUrl: './doctor-dashboard.component.html',
   styleUrls: ['./doctor-dashboard.component.css']
 })
--- src/app/services/DoctorServices/doctor-profile.service.ts
+++ src/app/services/DoctorServices/doctor-profile.service.ts
@@ -21,7 +21,7 @@ export class DoctorProfileService {
 
 
 
-  private availableSlotsUrl = `${environment.baseUrl}/Appointment/available-slotsDoctor`; // Endpoint for available slots
+  
   private availableSlotsUrlrelative = `${environment.baseUrl}/Appointment/available-slotsRelative`;
   private doctorPatientsUrl = `${environment.baseUrl}/PatientDoctor/DoctorPatients`;
   constructor(private http: HttpClient) {}
@@ -44,7 +44,7 @@ export class DoctorProfileService {
 
 
   getMaxAverageRating() : Observable<any>{
-    return this.http.get<any>(this.maxAverageRatingUrl);
+    return this.http.get<any>(this.maxAverageRatingUrl);}
 
   getDoctorPatients(): Observable<{ isSuccess: boolean; data: PatientNameDTO[] }> {
     return this.http.get<{ isSuccess: boolean; data: PatientNameDTO[] }>(this.doctorPatientsUrl);
