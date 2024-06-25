a7650889e5cc597338552521784354ef585e5039
Habeba-Adnan <Habebaadnan2020@gmail.com>
2024-06-25 22:43:01 +0300

    test

--- src/app/components/story-test/story-test.component.ts
+++ src/app/components/story-test/story-test.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { AfterViewInit, Component, OnInit, ViewChild ,ElementRef, TemplateRef} from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-//import { NgbModal } from '@ng-bootstrap/ng-bootstrap'; // Import NgbModal for modal functionality
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap'; // Import NgbModal for modal functionality
 import { StoryDTOs } from '../../models/Story/story-dtos';
 import { SharedModule } from '../../models/shared-module';
 declare const Plyr: any;
@@ -40,7 +40,7 @@ export class StoryTestComponent  implements OnInit, AfterViewInit {
 
 
   openModal(content: any) {
-   // this.modalService.open(content, { size: 'lg', centered: true });
+   this.modalService.open(content, { size: 'lg', centered: true });
 
   }
 
@@ -49,10 +49,10 @@ export class StoryTestComponent  implements OnInit, AfterViewInit {
   }
 
 
-    // Play the audio only when the user confirms understanding in the modal
+    Play the audio only when the user confirms understanding in the modal
     const audio = document.getElementById(audioId) as HTMLAudioElement;
     audio.onended = () => {
-      //this.modalService.dismissAll(); // Dismiss the modal when audio ends
+      this.modalService.dismissAll(); // Dismiss the modal when audio ends
     };
 
   playAudioOnce() {
@@ -82,41 +82,41 @@ export class StoryTestComponent  implements OnInit, AfterViewInit {
 
 
 
-// implements OnInit, AfterViewInit {
-// [x: string]: any;
-//   storyTest?: StoryDTOs;
+implements OnInit, AfterViewInit {
+[x: string]: any;
+  storyTest?: StoryDTOs;
 
-//   constructor(
-//     private route: ActivatedRoute,
-//     private router: Router,
-//     private modalService: NgbModal
-//   ) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private modalService: NgbModal
+  ) {}
 
-//   ngOnInit(): void {
-//     const navigation = this.router.getCurrentNavigation();
-//     if (navigation && navigation.extras.state) {
-//       this.storyTest = navigation.extras.state['storyTest'];
-//     } else {
-//       console.error('No storyTest data found in router state.');
-//     }
-//   }
+  ngOnInit(): void {
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation && navigation.extras.state) {
+      this.storyTest = navigation.extras.state['storyTest'];
+    } else {
+      console.error('No storyTest data found in router state.');
+    }
+  }
 
-//   ngAfterViewInit(): void {
-//     const player = new Plyr('#audioPlayer');
-//   }
+  ngAfterViewInit(): void {
+    const player = new Plyr('#audioPlayer');
+  }
 
-//   openModal(content: any) {
-//     this.modalService.open(content, { size: 'lg', centered: true });
-//   }
+  openModal(content: any) {
+    this.modalService.open(content, { size: 'lg', centered: true });
+  }
 
-//   playAudio(audioId: string, modalContent: any) {
-//     // Open the modal warning before playing the audio
-//     this.openModal(modalContent);
+  playAudio(audioId: string, modalContent: any) {
+    // Open the modal warning before playing the audio
+    this.openModal(modalContent);
 
-//     // Play the audio only when the user confirms understanding in the modal
-//     const audio = document.getElementById(audioId) as HTMLAudioElement;
-//     audio.onended = () => {
-//       this.modalService.dismissAll(); // Dismiss the modal when audio ends
-//     };
-//   }
-// }
+    // Play the audio only when the user confirms understanding in the modal
+    const audio = document.getElementById(audioId) as HTMLAudioElement;
+    audio.onended = () => {
+      this.modalService.dismissAll(); // Dismiss the modal when audio ends
+    };
+  }
+}
