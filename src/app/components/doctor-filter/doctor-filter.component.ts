c30e90164a94a0277ba2ec0fc733949292d800b0
Madonna Adel <madonnadel0@gmail.com>
2024-06-26 19:47:53 +0300

    add doctorFilter component

---
+++ src/app/components/doctor-filter/doctor-filter.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-filter',
+  standalone: true,
+  imports: [],
+  templateUrl: './doctor-filter.component.html',
+  styleUrl: './doctor-filter.component.css'
+})
+export class DoctorFilterComponent {
+
+}
