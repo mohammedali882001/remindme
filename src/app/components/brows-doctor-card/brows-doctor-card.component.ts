2161c39038ca694d208491e2fc1b7a3ba4dbae56
Madonna Adel <madonnadel0@gmail.com>
2024-06-27 16:35:03 +0300

    add brows doctor card

---
+++ src/app/components/brows-doctor-card/brows-doctor-card.component.ts
@@ -0,0 +1,17 @@
+import { Component } from "@angular/core";
+import { CustomCardComponent } from "../custom-card/custom-card.component";
+
+
+@Component({
+  selector: "app-brows-doctor-card",
+  standalone: true,
+  imports: [CustomCardComponent],
+  templateUrl: "./brows-doctor-card.component.html",
+  styleUrl: "./brows-doctor-card.component.css",
+})
+export class BrowsDoctorCardComponent {
+
+
+
+ 
+}
