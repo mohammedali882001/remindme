68e0e539aaf552df62f2a576c5f71df2635b9960
Habeba-Adnan <Habebaadnan2020@gmail.com>
2024-07-08 00:49:26 +0300

    test

--- src/app/pages/DoctorProfile/doctorprofile/doctorprofile.component.ts
+++ src/app/pages/DoctorProfile/doctorprofile/doctorprofile.component.ts
@@ -14,7 +14,6 @@ import { environment } from '../../../../environments/environment.development';
 import { DoctorSidebarComponent } from "../../../components/doctor-sidebar/doctor-sidebar.component";
 
 import Swal from 'sweetalert2';
-import { DoctorSidebarComponent } from "../../../components/doctor-sidebar/doctor-sidebar.component";
 
 
 @Component({
